8537a1ce1e694749bcb19c123cfd09bc4f98255e
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-04-26 16:53:40 -0700

    Build before running

--- run.ps1
+++ run.ps1
@@ -1,3 +1,24 @@
 # We assume we're in the project root directory
 
-java -jar build/program.jar $ARGS
+$programargs = @()
+
+$build = $true
+
+for ([int]$i = 0; $i -lt $ARGS.Length; ++$i) {
+	$arg = $ARGS[$i]
+	if ($arg -eq '-Sskipbuild') {
+		$build = $false
+	} elseif ($arg -clike '-S*') {
+		echo "Got unknown script argument '$arg'"
+		return 1
+	} else {
+		$programargs = $ARGS[$i..($ARGS.Length-1)]
+		break
+	}
+}
+
+if ($build) {
+	./build
+}
+
+java -jar build/program.jar $programargs
