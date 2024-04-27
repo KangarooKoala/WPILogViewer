dde71764a8fd22c48bb9347d395f9e39ce1b11fd
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-04-26 17:18:40 -0700

    Propagate verbosity to build, add -Sq, check run verbosity

--- run.ps1
+++ run.ps1
@@ -3,14 +3,24 @@
 [string[]]$programargs = @()
 
 [bool]$build = $true
-# 1: normal; 2: verbose
+# 0: quiet; 1: normal; 2: verbose
 [int]$verbosity = 1
 
 for ([int]$i = 0; $i -lt $ARGS.Length; ++$i) {
 	[string]$arg = $ARGS[$i]
 	if ($arg -eq '-Sskipbuild') {
 		$build = $false
+	} elseif ($arg -eq '-Sq' -or $arg -eq '-Squiet') {
+		if ($verbosity -gt 1) {
+			echo 'Cannot specify -Sq with -Sv'
+			exit 1
+		}
+		$verbosity = 0
 	} elseif ($arg -eq '-Sv' -or $arg -eq '-Sverbose') {
+		if ($verbosity -lt 1) {
+			echo 'Cannot specify -Sv with -Sq'
+			exit 1
+		}
 		$verbosity = 2
 	} elseif ($arg -clike '-S*') {
 		echo "Got unknown script argument '$arg'"
@@ -21,11 +31,24 @@ for ([int]$i = 0; $i -lt $ARGS.Length; ++$i) {
 	}
 }
 
+if ($verbosity -notin @(0, 1, 2)) {
+	echo "WARNING: Unknown verbosity $verbosity! Running anyways"
+}
+
 if ($build) {
 	if ($verbosity -ge 1) {
 		echo 'Building'
 	}
-	./build
+	if ($verbosity -eq 0) {
+		./build -q
+	} elseif ($verbosity -eq 1) {
+		./build
+	} elseif ($verbosity -eq 2) {
+		./build -v
+	} else {
+		# We already warned the user
+		./build
+	}
 }
 
 if ($verbosity -ge 2) {
