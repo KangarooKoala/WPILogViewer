9f33e45b71103ec2c21d36adaf8f589d26b66a0b
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-04-26 17:08:29 -0700

    Add logging and verbosity parameters

--- build.ps1
+++ build.ps1
@@ -1,11 +1,53 @@
 # We assume we're in the project root directory
 
+# 0: quiet; 1: normal; 2: verbose
+$verbosity = 1
+
+foreach ($arg in $ARGS) {
+	if ($arg -eq '-q') {
+		if ($verbosity -gt 1) {
+			echo 'Cannot specify -q with -v'
+			exit 1
+		}
+		$verbosity = 0
+	} elseif ($arg -eq '-v') {
+		if ($verbosity -lt 1) {
+			echo 'Cannot specify -v with -q'
+			exit 1
+		}
+		$verbosity = 2
+	} else {
+		echo "Unknown option '$arg'"
+		exit 1
+	}
+}
+
+if ($verbosity -ge 2) {
+	echo 'Setting up build directory'
+}
+
 mkdir -force build/class | out-null
 
 rm -r build/class/*
 
+if ($verbosity -ge 2) {
+	echo 'Loading source files'
+}
+
 $srcfiles = ls -r -file src/*.java | % { $_.ToString() }
+
+if ($verbosity -ge 1) {
+	echo "Compiling $($srcfiles.Length) files"
+}
 
 javac -d build/class $srcfiles
 
+if ($verbosity -ge 1) {
+	echo 'Generating jar'
+}
+
 jar --create --file build/program.jar --main-class=wpilogviewer.Main -C build/class/ .
+
+if ($verbosity -ge 1) {
+	echo 'Done'
+}
--- run.ps1
+++ run.ps1
@@ -3,11 +3,15 @@
 $programargs = @()
 
 $build = $true
+# 1: normal; 2: verbose
+$verbosity = 1
 
 for ([int]$i = 0; $i -lt $ARGS.Length; ++$i) {
 	$arg = $ARGS[$i]
 	if ($arg -eq '-Sskipbuild') {
 		$build = $false
+	} elseif ($arg -eq '-Sv' -or $arg -eq '-Sverbose') {
+		$verbosity = 2
 	} elseif ($arg -clike '-S*') {
 		echo "Got unknown script argument '$arg'"
 		return 1
@@ -18,7 +22,14 @@ for ([int]$i = 0; $i -lt $ARGS.Length; ++$i) {
 }
 
 if ($build) {
+	if ($verbosity -ge 1) {
+		echo 'Building'
+	}
 	./build
+}
+
+if ($verbosity -ge 2) {
+	echo "Running program with arguments '$programargs'"
 }
 
 java -jar build/program.jar $programargs
