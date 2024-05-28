e19158f2e8d78a89f1eb0ca61f89f7927daecbee
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-05-28 14:59:04 -0700

    Add hash checking to build

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@
 [int]$verbosity = 1
 [bool]$setverbosity = $false
 
+[bool]$checkhash = $true
+
 foreach ($arg in $ARGS) {
 	if ($arg -ceq '-q') {
 		if ($setverbosity) {
@@ -40,10 +42,61 @@ foreach ($arg in $ARGS) {
 			echo "WARNING: Building with nonstandard verbosity $verbosity"
 		}
 		$setverbosity = $true
+	} elseif ($arg -ceq '-skiphash') {
+		$checkhash = $false
 	} else {
 		echo "Unknown option '$arg'"
 		exit 1
 	}
+}
+
+if ($verbosity -ge 2) {
+	echo 'Loading source files'
+}
+
+[string[]]$srcfiles = ls -r -file src/*.java | % { $_.ToString() }
+
+if (-not $checkhash) {
+	if ($verbosity -ge 2) {
+		echo 'Skipping hash check'
+	}
+} else {
+	if ($verbosity -ge 2) {
+		echo 'Generating hashes'
+	}
+
+	[string[]]$newhashes = get-filehash $srcfiles -algorithm SHA256 | % { "$($_.Path): $($_.Hash)" }
+
+	if (test-path build/hashes) {
+		if ($verbosity -ge 2) {
+			echo 'Checking hashes'
+		}
+		[string[]]$oldhashes = cat build/hashes
+		# PowerShell -eq behaves differently with collections
+		[bool]$hashesmatch = $true
+		if ($newhashes.Length -ne $oldhashes.Length) {
+			$hashesmatch = $false
+		} else {
+			for ([int]$i = 0; $i -lt $newhashes.Length; ++$i) {
+				if ($newhashes[$i] -ne $oldhashes[$i]) {
+					$hashesmatch = $false
+					break
+				}
+			}
+		}
+		if ($hashesmatch) {
+			if ($verbosity -ge 1) {
+				echo 'Source file hashes match, skipping compilation'
+			}
+			exit 0
+		}
+	}
+
+	if ($verbosity -ge 2) {
+		echo 'Writing hashes'
+	}
+
+	$newhashes > build/hashes
 }
 
 if ($verbosity -ge 2) {
@@ -53,12 +106,6 @@ if ($verbosity -ge 2) {
 mkdir -force build/class | out-null
 
 rm -r build/class/*
-
-if ($verbosity -ge 2) {
-	echo 'Loading source files'
-}
-
-[string[]]$srcfiles = ls -r -file src/*.java | % { $_.ToString() }
 
 if ($verbosity -ge 1) {
 	echo "Compiling $($srcfiles.Length) files"
